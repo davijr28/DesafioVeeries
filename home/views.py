5147ca43410d02ff6ecdb341c8ec8894fc5a8765
davijr28 <davijr28@gmail.com>
2025-09-14 12:29:58 -0300

    fix: atualiza e corrige erros de exibição na tabela

--- home/views.py
+++ home/views.py
@@ -1,8 +1,6 @@
-from django.db.models import Sum
 from django.utils.timezone import now
 from django.shortcuts import render
 from home.models import NavioSilver
-from datetime import timedelta
 
 
 def home(request):
@@ -12,22 +10,15 @@ def home(request):
 def gold_volume_diario(request):
     hoje = now().astimezone().date()
 
-    dados = (
-        NavioSilver.objects.filter(data_chegada__date__lte=hoje)
-        .values("data_chegada", "produto", "porto", "sentido")
-        .annotate(volume_total=Sum("volume"))
-        .order_by("-data_chegada", "porto", "produto", "sentido")
+    dados = NavioSilver.objects.filter(data_chegada__date__lte=hoje).order_by(
+        "-data_chegada", "porto", "produto", "sentido"
     )
 
-    santos = [d for d in dados if d["porto"] == "Porto de Santos"]
-    paranagua = [d for d in dados if d["porto"] == "Porto de Paranaguá"]
+    santos = [d for d in dados if d.porto == "Porto de Santos"]
+    paranagua = [d for d in dados if d.porto == "Porto de Paranaguá"]
 
-    volume_santos = sum(
-        d["volume_total"] for d in santos if d["data_chegada"].date() == hoje
-    )
-    volume_paranagua = sum(
-        d["volume_total"] for d in paranagua if d["data_chegada"].date() == hoje
-    )
+    volume_santos = sum(d.volume for d in santos if d.data_chegada.date() == hoje)
+    volume_paranagua = sum(d.volume for d in paranagua if d.data_chegada.date() == hoje)
 
     return render(
         request,
